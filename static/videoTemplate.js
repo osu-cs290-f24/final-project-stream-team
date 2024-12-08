fa8fc38a12402787013691782d4d1ce4c3995854
Gage P Shinneman <156984085+gageshinneman@users.noreply.github.com>
2024-12-07 17:18:30 -0800

    changed layout of posts

--- static/videoTemplate.js
+++ static/videoTemplate.js
@@ -8,16 +8,16 @@ templates['video'] = template({"compiler":[8,">= 4.3.0"],"main":function(contain
         return undefined
     };
 
-  return "<div id=\"video-container\">\r\n    <div class=\"video-post\">\r\n        <img src=\""
+  return "<div id=\"video-container\">\n    <div class=\"video-post\">\n        <img src=\""
     + alias4(((helper = (helper = lookupProperty(helpers,"photoURL") || (depth0 != null ? lookupProperty(depth0,"photoURL") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"photoURL","hash":{},"data":data,"loc":{"start":{"line":3,"column":18},"end":{"line":3,"column":30}}}) : helper)))
     + "\" alt=\""
     + alias4(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":3,"column":37},"end":{"line":3,"column":46}}}) : helper)))
-    + "\">\r\n        <h3>"
+    + "\">\n        <h3>"
     + alias4(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":4,"column":12},"end":{"line":4,"column":21}}}) : helper)))
-    + "</h3>\r\n        <p>By: "
+    + "</h3>\n        <p>By: "
     + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":5,"column":15},"end":{"line":5,"column":23}}}) : helper)))
-    + "</p>\r\n        <p>Length: "
+    + "</p>\n        <p>Length: "
     + alias4(((helper = (helper = lookupProperty(helpers,"length") || (depth0 != null ? lookupProperty(depth0,"length") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"length","hash":{},"data":data,"loc":{"start":{"line":6,"column":19},"end":{"line":6,"column":29}}}) : helper)))
-    + " minutes</p>\r\n    </div>\r\n</div>";
+    + " minutes</p>\n    </div>\n</div>";
 },"useData":true});
 })();
